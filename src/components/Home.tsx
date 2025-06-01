a4f2c8fc3fbd92137e1d6d115c059804b961e7f7
NebulaEric <war71624@ttu.edu>
2025-06-01 13:24:34 -0500

    Nav Bar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import reactLogo from '/src/assets/react.svg'
 import viteLogo from '/vite.svg'
+import profileLogo from '/src/assets/EW.png'
 import './Home.css'
 import { useState } from 'react'
 import { Outlet } from "react-router-dom"
@@ -8,6 +9,25 @@ import { Outlet } from "react-router-dom"
 export const Home: React.FC = () => {
     const [count, setCount] = useState(0)
     return <>
+        <header>
+        <nav>
+            <div className="container nav">
+                <a className="logo" href="index.html"><img src={profileLogo} alt="logo"/>Eric's Projects</a>
+
+                <button className="hamburger">☰</button>
+
+  {/* <!-- Navigation Links --> */}
+                <div className="navLinks">
+                    <a className="button" href="#hello">update</a>
+                    <a className="button" href="#about">About</a>
+                    <a className="button" href="#education">Education</a>
+                    <a className="button" href="#project">Projects</a>
+                    <a className="button" href="#contact">Contact</a>
+                </div>
+            
+            </div>
+        </nav>
+      </header>
         <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
